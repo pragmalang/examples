1e33489821b6fd9a4973253b841bdec8563d9a7e
Tabzz98 <muhammadtabaza@gmail.com>
2020-10-28 00:43:42 +0300

    Formatting

--- introduction/hooks.js
+++ introduction/hooks.js
@@ -1,11 +1,11 @@
 const validateUser = user => {
-  if(user.age < 18) {
+  if (user.age < 18) {
     throw new Error("Age must be over 18")
   }
   return user
 }
 
-const setFullName = user => 
-  ({...user, fullName: user.firstName + " " + user.lastName})
+const setFullName = user =>
+  ({ ...user, fullName: user.firstName + " " + user.lastName })
 
 module.exports = { validateUser, setFullName }
